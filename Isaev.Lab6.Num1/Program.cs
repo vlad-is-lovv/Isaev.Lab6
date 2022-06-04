44dfea233fc15e88d08a5b9089e80d8d89ad98ef
arinameliakina <melyakina2003@mail.ru>
2022-06-04 12:19:12 +0300

    Изменения в коде Добавьте файлы проекта.Изменения в кодеДобавьте файлы проекта.

---
+++ Isaev.Lab6.Num1/Program.cs
@@ -0,0 +1,38 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[] Q = new int[n];
+Random(Q);
+Show(Q);
+int quantity = 0;
+int sum = 0;
+
+for (int i = 0; i < Q.Length; i++)
+{
+    if (Q[i] % 7 != 0 && Q[i] % 5 ==0)
+    {
+        quantity += 1;
+        sum+=Q[i];
+    }
+}   
+Console.WriteLine($"Количество чисел: {quantity}. Cумма чисел: {sum}.");
+
+
+
+static void Show(int[] array)
+{
+    for (var i = 0; i < array.Length; i++)
+    {
+
+        Console.Write($"{array[i]}" + "  ");
+
+    }
+
+}
+ static void Random(int[] array)
+{
+    Random r = new Random();
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = r.Next(0, 100);
+    }
+
+}
---
+++ Isaev.Lab6.Num2/Program.cs
@@ -0,0 +1,52 @@
+﻿ static void Show(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0,-3}" + "  ", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+ static void Random(int[,] array)
+{
+    Random r = new Random();
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = r.Next(-100, 100);
+        }
+    }
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] Z = new int[n, n];
+
+Random(Z);
+
+Show(Z);
+
+for (int i = 0;i < Z.GetLength(0); i++ )
+ { int val = Z[i, 0];
+    int val_j = 0;
+    for (int j = 0; j < Z.GetLength(1); j++)
+    {
+        if (Z[i, j] > val)
+        {
+            val = Z[i, j];
+            val_j = j;
+        }
+    }
+
+    if (Z[i, i] != val)
+    { 
+     Z[i,val_j] = Z[i,i];
+        Z[i, i] = val;
+    }
+}
+Console.WriteLine();
+Show(Z);
